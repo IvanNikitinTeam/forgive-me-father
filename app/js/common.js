5e4c290c9eb99a8b963a2fc48978805e15b3b440
Ivan Karamanskyi <ivan@nikitinteam.com>
2024-11-15 00:06:42 +0200

    Add favicon

--- app/js/common.js
+++ app/js/common.js
@@ -19,46 +19,52 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	startRandomizingChars();
 
-
-		const timer = document.querySelector(".hero__timer");
-		const targetDateStr = timer.getAttribute("data-target-date");
-		const targetDateParts = targetDateStr.split('/');
-
-		const targetDate = new Date(`${targetDateParts[2]}-${targetDateParts[1]}-${targetDateParts[0]}T00:00:00`);
-
-		function formatNumber(number, isDay) {
-				if (isDay) {
-						return number;
-				}
-				return number < 10 ? `0${number}` : number;
+	const timer = document.querySelector(".hero__timer");
+	const targetDateStr = timer.getAttribute("data-target-date");
+	const targetDateParts = targetDateStr.split('/');
+	
+	// Встановлюємо цільову дату у форматі UTC
+	const targetDate = new Date(Date.UTC(
+		parseInt(targetDateParts[2], 10), // рік
+		parseInt(targetDateParts[1], 10) - 1, // місяць (0-11)
+		parseInt(targetDateParts[0], 10), // день
+		0, 0, 0 // час 00:00:00
+	));
+	
+	function formatNumber(number, isDay) {
+		if (isDay) {
+			return number;
 		}
-
-		function updateTimer() {
-				const currentTime = new Date();
-				const timeDiff = targetDate - currentTime;
-
-				if (timeDiff <= 0) {
-						clearInterval(timerInterval);
-						timer.querySelector(".hero__timer-item-num.days").textContent = '00';
-						timer.querySelector(".hero__timer-item-num.hours").textContent = '00';
-						timer.querySelector(".hero__timer-item-num.minutes").textContent = '00';
-						timer.querySelector(".hero__timer-item-num.seconds").textContent = '00';
-						return;
-				}
-
-				const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-				const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-				const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-				const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
-
-				timer.querySelector(".hero__timer-item-num.days").textContent = formatNumber(days, true); // Для днів не форматувати
-				timer.querySelector(".hero__timer-item-num.hours").textContent = formatNumber(hours, false);
-				timer.querySelector(".hero__timer-item-num.minutes").textContent = formatNumber(minutes, false);
-				timer.querySelector(".hero__timer-item-num.seconds").textContent = formatNumber(seconds, false);
+		return number < 10 ? `0${number}` : number;
+	}
+	
+	function updateTimer() {
+		const currentTimeUTC = new Date().getTime(); // Поточний час в мс за UTC
+		const timeDiff = targetDate - currentTimeUTC;
+	
+		if (timeDiff <= 0) {
+			clearInterval(timerInterval);
+			timer.querySelector(".hero__timer-item-num.days").textContent = '00';
+			timer.querySelector(".hero__timer-item-num.hours").textContent = '00';
+			timer.querySelector(".hero__timer-item-num.minutes").textContent = '00';
+			timer.querySelector(".hero__timer-item-num.seconds").textContent = '00';
+			return;
 		}
-
-		const timerInterval = setInterval(updateTimer, 1000);
-		updateTimer();
+	
+		const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+		const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+		const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+	
+		timer.querySelector(".hero__timer-item-num.days").textContent = formatNumber(days, true);
+		timer.querySelector(".hero__timer-item-num.hours").textContent = formatNumber(hours, false);
+		timer.querySelector(".hero__timer-item-num.minutes").textContent = formatNumber(minutes, false);
+		timer.querySelector(".hero__timer-item-num.seconds").textContent = formatNumber(seconds, false);
+	}
+	
+	const timerInterval = setInterval(updateTimer, 1000);
+	updateTimer();
+	
 
 
 	//prevent drag img and a
