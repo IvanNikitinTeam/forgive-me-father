895a583771d5722bc5675f5d59fc43d7ad838113
Ivan Karamanskyi <ivan@nikitinteam.com>
2024-11-14 21:31:19 +0200

    add fadeIn

--- app/js/common.js
+++ app/js/common.js
@@ -1,69 +1,65 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-	// loader
-	setTimeout(function () {
-		document.querySelector('body').classList.remove('loaded');
-	}, 400);
+	new WOW().init();
 
-	// lazy load
-	// setTimeout(function () {
-	// 	$(".js-bg").each(function () {
-	// 		$(this).css('background-image', 'url(' + $(this).data("bg") + ')');
-	// 	});
-	// 	$(".js-img").each(function () {
-	// 		$(this).attr('src', $(this).data("src"));
-	// 	});
-	// }, 200);
+	function getRandomChar() {
+		const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?';
+		return chars.charAt(Math.floor(Math.random() * chars.length));
+	}
+
+	function startRandomizingChars() {
+			const charElements = document.querySelectorAll('span.char');
+
+			setInterval(() => {
+					charElements.forEach(element => {
+							element.textContent = getRandomChar();
+					});
+			}, 150);
+	}
+
+	startRandomizingChars();
 
 
+		const timer = document.querySelector(".hero__timer");
+		const targetDateStr = timer.getAttribute("data-target-date");
+		const targetDateParts = targetDateStr.split('/');
 
-	/* components */
+		const targetDate = new Date(`${targetDateParts[2]}-${targetDateParts[1]}-${targetDateParts[0]}T00:00:00`);
 
-	/*
-	if($('.styled').length) {
-		$('.styled').styler();
-	};
-	if($('.fancybox').length) {
-		$('.fancybox').fancybox({
-			margin  : 10,
-			padding  : 10
-		});
-	};
-	if($('.slick-slider').length) {
-		$('.slick-slider').slick({
-			dots: true,
-			infinite: false,
-			speed: 300,
-			slidesToShow: 4,
-			slidesToScroll: 4,
-			responsive: [
-				{
-				  breakpoint: 1024,
-				  settings: {
-					slidesToShow: 3,
-					slidesToScroll: 3,
-					infinite: true,
-					dots: true
-				  }
-				},
-				{
-				  breakpoint: 600,
-				  settings: "unslick"
-				}				
-			]
-		});
-	};
-	if($('.scroll').length) {
-		$(".scroll").mCustomScrollbar({
-			axis:"x",
-			theme:"dark-thin",
-			autoExpandScrollbar:true,
-			advanced:{autoExpandHorizontalScroll:true}
-		});
-	};
-	*/
+		function formatNumber(number, isDay) {
+				if (isDay) {
+						return number;
+				}
+				return number < 10 ? `0${number}` : number;
+		}
 
-	/* components */
+		function updateTimer() {
+				const currentTime = new Date();
+				const timeDiff = targetDate - currentTime;
+
+				if (timeDiff <= 0) {
+						clearInterval(timerInterval);
+						timer.querySelector(".hero__timer-item-num.days").textContent = '00';
+						timer.querySelector(".hero__timer-item-num.hours").textContent = '00';
+						timer.querySelector(".hero__timer-item-num.minutes").textContent = '00';
+						timer.querySelector(".hero__timer-item-num.seconds").textContent = '00';
+						return;
+				}
+
+				const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+				const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+				const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+				const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+				timer.querySelector(".hero__timer-item-num.days").textContent = formatNumber(days, true); // Для днів не форматувати
+				timer.querySelector(".hero__timer-item-num.hours").textContent = formatNumber(hours, false);
+				timer.querySelector(".hero__timer-item-num.minutes").textContent = formatNumber(minutes, false);
+				timer.querySelector(".hero__timer-item-num.seconds").textContent = formatNumber(seconds, false);
+		}
+
+		const timerInterval = setInterval(updateTimer, 1000);
+		updateTimer();
+
 
 	//prevent drag img and a
 	const imagesAndLinks = document.querySelectorAll('img, a');
@@ -76,30 +72,6 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
 	
-	const handlerResize = function () {
-		let viewport_wid = viewport().width;
-		let viewport_height = viewport().height;
-
-		// if (viewport_wid <= 991) {
-			
-		// }
-	}
-
-	window.addEventListener('load', handlerResize);
-	window.addEventListener('resize', handlerResize);
 
 
 });
-
-/* viewport width */
-function viewport() {
-	let e = window,
-		a = 'inner';
-	if (!('innerWidth' in window)) {
-		a = 'client';
-		e = document.documentElement || document.body;
-	}
-	return { width: e[a + 'Width'], height: e[a + 'Height'] }
-};
-/* viewport width */
-
--- app/libs/libs.js
+++ app/libs/libs.js
@@ -1,1 +1,1 @@
-//= jquery/jquery-3.4.1.min.js
+//= wow/wow.min.js
